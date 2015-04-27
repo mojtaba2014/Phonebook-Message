16d380e4832bb3ef542609bc1accaef5902018b0
baratoff <johann84bjarni@gmail.com>
2015-04-27 11:43:54 +0200

    tree

--- Phonebook/Controll layer/DBConection.cs
+++ Phonebook/Controll layer/DBConection.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Phonebook
 {
     class DBConection
     {
+        SqlConnection Conn = new SqlConnection("Server=ealdb1.eal.local;" +
+                                                     "Database=EJL87_DB;" +
+                                                     "User ID=ejl87_usr;" +
+                                                    "Password=Baz1nga87;");
        public void EditContactDB(Contact C)
        {
 
@@ -18,7 +24,36 @@ namespace Phonebook
        }
        public List<Contact> GetPhonebook()
        {
+           List<Contact> Clist = new List<Contact>();
+           try
+           {
+               Conn.Open();
+               SqlCommand cmd = new SqlCommand("GetAllContacts", Conn);
+               cmd.CommandType = CommandType.StoredProcedure;
 
-       }
+               SqlDataReader rdr = cmd.ExecuteReader();
+               while (rdr.HasRows && rdr.Read())
+               {
+
+                   Contact C = new Contact(rdr["name"].ToString(), Int32.Parse(rdr["workPhone"].ToString()), Int32.Parse(rdr["privatePhone"].ToString()), rdr["CompanyName"].ToString());
+
+                   Clist.Add(C);
+
+               }
+              
+
+             
+           }
+           catch (SqlException e)
+           {
+               Console.WriteLine("conection error " + e.Message);
+           }
+           finally
+           {
+               Conn.Close();
+               Conn.Dispose();
+           }
+         }
+       
     }
 }
